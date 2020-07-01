6712ac251244bed24553c0349f5ca2b243cba6f6
unknown <negru.cristina.g@gmail.com>
2020-07-01 07:12:15 +0300

    homework3

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a=3;
+	int b=5;
+	int c=4;
+        System.out.println(a +" "+b+ " "+c+" ");
+
+
+        System.out.println( );
+        int a1=512343;
+        int b1=3431231;
+        System.out.println( a1+b1);
+
+        System.out.println( );
+        int d=123456;
+        System.out.println(d);
+
+    }
+}
